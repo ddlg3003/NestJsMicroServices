58df744e811df017dee4c7338af5d722ab1d6c7d
NGUYỄN HỮU ĐĂNG <ducbmt2009@gmail.com>
2023-04-08 15:31:29 +0700

    fix

--- apps/worker-update/src/utils/globalVariables.ts
+++
@@ -1,6 +0,0 @@
-export const TODO_EXCHANGE = 'todo_exchange';
-
-export enum ROUTING_KEY {
-  CREATE = 'create',
-  UPDATE = 'update',
-}
--- apps/worker/src/worker.service.ts
+++ apps/worker/src/worker.service.ts
@@ -92,4 +92,39 @@ export class WorkerService {
       body: todo,
     }).catch(err => console.log('Something happen with indexing. Err: ', err));
   }
+
+  @RabbitSubscribe({
+    exchange: TODO_EXCHANGE,
+    routingKey: ROUTING_KEY.UPDATE,
+    queueOptions: {
+      exclusive: true,
+    },
+  })
+  updateTodoIndex(todo: Todo) {
+    // ctx._source.title='sleep';
+    const updateScript = Object
+      .entries(todo)
+      .reduce((script, [key, val]) => {
+        if(key === 'isCompleted') {
+          script += ` ctx._source.${key}=${val};`;
+        } else {
+          script += ` ctx._source.${key}='${val}';`;
+        }
+        return script;
+      }, '');
+
+    this.elasticSearchService.updateByQuery({
+      index: this.configService.get<string>('INDEX'),
+      body: {
+        query: {
+          match: {
+            id: todo.id,
+          },
+        },
+        script: {
+          inline: updateScript,
+        },
+      },
+    }).catch(err => console.log('Something happen with update index. Err: ', err));
+  }
 }
